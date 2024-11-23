c42e74193f9fa9dcd3a2d0cc6d13379eb28173d6
nattanjunior <fnatanieljunior@gmail.com>
2024-11-23 17:46:46 -0300

    editadno video e processando video com ffmpeg, tratando erros, enviando resposta ao client e encerrando processo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createServer } from "http";
 import { createReadStream } from "fs";
+import { spawn } from "child_process";
 
 const port = 3000;
 
@@ -14,7 +15,36 @@ createServer(async (req, res) => {
     return;
   }
 
-  createReadStream('./assets/video.mp4')
-    .pipe(res)
+  res.writeHead(200, {
+    'Content-Type': 'video/mp4'
+  })
+
+  const ffmpegProcess = spawn('ffmpeg', [
+    '-i', 'pipe:0',
+    '-f', 'mp4',
+    '-vcodec', 'h264',
+    '-acodec', 'aac',
+    '-movflags', 'frag_keyframe+empty_moov+default_base_moof',
+    '-b:v', '1500k',
+    '-maxrate', '1500k',
+    '-bufsize', '1000k',
+    '-f', 'mp4',
+    '-vf', "drawtext=text='racionais@gmail.com':x=10:y=H-th-10:fontsize=50:fontcolor=red:shadowcolor=red:shadowx=5",
+    'pipe:1'
+  ], {
+    stdio: ['pipe', 'pipe', 'pipe']
+  })
+
+  createReadStream('./assets/video-ready.mp4').pipe(ffmpegProcess.stdin)
+
+  ffmpegProcess.stderr.on('data', msg => console.log(msg.toString()))
+  ffmpegProcess.stdout.pipe(res)
+
+  req.once('close', () => {
+    ffmpegProcess.stdout.destroy();
+    ffmpegProcess.stdin.destroy();
+    console.log('disconnected', ffmpegProcess.kill())
+  })
+
 })
   .listen(port, () => console.log('server is running at 3000'))
